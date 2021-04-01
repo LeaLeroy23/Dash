590c4631e7a8fc1f06a8559ae82d36e22e8fe16f
LeaLeroy23 <lea.leroy23@gmail.com>
2021-04-01 16:00:02 +0200

    last changes

--- wp-content/themes/test/index.php
+++ wp-content/themes/test/index.php
@@ -101,7 +101,7 @@
         <div class="section-form col-12 col-sm-12 col-lg-8 mx-auto">
             <div class="row">
                 <div class="col-7 col-sm-7 col-md-7 col-lg-7">
-                    <input id="input-footer-email" type="email" class="form-control" placeholder="Name@company.com">
+                    <input id="input-footer-email" type="email" class="form-control" name="" placeholder="Name@company.com">
                 </div>
                 <div class="col-5 col-sm-5 col-md-5 col-lg-5">
                     <button class="btn" id="btn-footer-orange"><?= get_field('button_text') ?></button>
