961df8072c3dc800aa88f023d04a7e5d8caf3406
LeaLeroy23 <lea.leroy23@gmail.com>
2021-04-01 14:57:38 +0200

    revue code

--- wp-content/themes/test/index.php
+++ wp-content/themes/test/index.php
@@ -4,11 +4,12 @@
 <section id="hero">
     <div class="container">
         <header id="header" class="d-flex row">
-            <div class="col-8 col-md-11 col-lg-10">
+            <div class="col-8 col-md-9 col-lg-10">
                 <h1 class="text-dark dash-title"><?= get_field('title') ?></h1>
             </div>
             <div class="col-4 col-md-3 col-lg-2 header-button">
-                <button type="button" class="btn btn-outline-light" id="btn-radius"><?= get_field('button_text') ?></button>
+                <button type="button" class="btn btn-outline-light"
+                    id="btn-radius"><?= get_field('button_text') ?></button>
             </div>
         </header>
         <div class="d-flex">
@@ -25,21 +26,30 @@
         <div class="d-flex block-partner">
             <div class="col-12 col-sm-12 col-md-10 col-lg-8">
                 <div class="partner-row d-flex justify-content-between">
-                    <img class="logo-partner grayscale" src="http://localhost/test/wp-content/uploads/2021/03/1200px-Cartoon_Network_2010_logo.svg_.png">
-                    <img class="logo-partner grayscale" src="http://localhost/test/wp-content/uploads/2021/04/Logo-Booking.com_.png">
-                    <img class="logo-partner grayscale" src="http://localhost/test/wp-content/uploads/2021/03/1200px-Dropbox_logo_2017.svg_.png">
+                    <img class="logo-partner grayscale"
+                        src="http://localhost/test/wp-content/uploads/2021/03/1200px-Cartoon_Network_2010_logo.svg_.png">
+                    <img class="logo-partner grayscale"
+                        src="http://localhost/test/wp-content/uploads/2021/04/Logo-Booking.com_.png">
+                    <img class="logo-partner grayscale"
+                        src="http://localhost/test/wp-content/uploads/2021/03/1200px-Dropbox_logo_2017.svg_.png">
                 </div>
 
                 <div class="partner-row d-flex justify-content-between">
-                    <img class="logo-partner grayscale" src="http://localhost/test/wp-content/uploads/2021/03/1024px-Toshiba_logo.svg_.png">
-                    <img class="logo-partner grayscale" src="http://localhost/test/wp-content/uploads/2021/04/Slack_logo_new.png">
-                    <img class="logo-partner grayscale" src="http://localhost/test/wp-content/uploads/2021/04/Netflix-logo.png">
+                    <img class="logo-partner grayscale"
+                        src="http://localhost/test/wp-content/uploads/2021/03/1024px-Toshiba_logo.svg_.png">
+                    <img class="logo-partner grayscale"
+                        src="http://localhost/test/wp-content/uploads/2021/04/Slack_logo_new.png">
+                    <img class="logo-partner grayscale"
+                        src="http://localhost/test/wp-content/uploads/2021/04/Netflix-logo.png">
                 </div>
 
                 <div class="partner-row d-flex justify-content-between">
-                    <img class="logo-partner grayscale" src="http://localhost/test/wp-content/uploads/2021/04/spotify-logo-png-7.png">
-                    <img class="logo-partner grayscale" src="http://localhost/test/wp-content/uploads/2021/03/Coca-Cola-logo.png">
-                    <img class="logo-partner" src="http://localhost/test/wp-content/uploads/2021/03/red-bull-logo-2-1.png">
+                    <img class="logo-partner grayscale"
+                        src="http://localhost/test/wp-content/uploads/2021/04/spotify-logo-png-7.png">
+                    <img class="logo-partner grayscale"
+                        src="http://localhost/test/wp-content/uploads/2021/03/Coca-Cola-logo.png">
+                    <img class="logo-partner"
+                        src="http://localhost/test/wp-content/uploads/2021/03/red-bull-logo-2-1.png">
                 </div>
 
             </div>
@@ -75,6 +85,35 @@
     </div>
 
 </section>
+
+<!-- Start Section Footer -->
+
+<section id="footer">
+    <div class="container">
+
+        <div class="row">
+            <div class="col-sm-9 section-title mx-auto">
+                <h3 id="title-footer"><?= get_field('title_footer') ?></h3>
+                <p class="text-footer"><?= get_field('text_footer') ?></p>
+            </div>
+        </div>
+
+        <div class="section-form col-12 col-sm-12 col-lg-8 mx-auto">
+            <div class="row">
+                <div class="col-7 col-sm-7 col-md-7 col-lg-7">
+                    <input id="input-footer-email" type="email" class="form-control" placeholder="Name@company.com">
+                </div>
+                <div class="col-5 col-sm-5 col-md-5 col-lg-5">
+                    <button class="btn" id="btn-footer-orange"><?= get_field('button_text') ?></button>
+                </div>
+
+            </div>
+        </div>
+
+    </div>
+</section>
+
+<!-- End SectionFooter -->
 
 <!-- End Section benefits -->
 
